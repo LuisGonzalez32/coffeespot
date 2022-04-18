9ba4c0eaa8790db06d1e27fd8280a3115aa54eb6
MauricioC10 <Mauri_c1028@hotmail.com>
2022-04-18 01:18:11 -0600

    Recursos

---
+++ models/recursos.py
@@ -0,0 +1,15 @@
+from utils.db import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+ 
+
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+        
+    def __repr__(self) -> str:
+        return f"Recursos({self.id}, {self.username}, '{self.password}'"
